a49a1193b720ebf1a23d4ce2a304b3edfbfd3466
chukazee <chukazelibe@gmail.com>
2022-09-08 16:31:36 -0700

    q4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 /**
  *
  * * main - Printing
@@ -9,8 +11,13 @@
  * * Return: Always 0 (Successful)
  *
  * */
+
 int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
