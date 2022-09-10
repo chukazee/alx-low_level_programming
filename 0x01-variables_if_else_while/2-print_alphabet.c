2f0c8aebaa0f4bb322e0aa60a271104813438302
chukazee <chukazelibe@gmail.com>
2022-09-09 21:51:16 -0700

    final

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,15 +9,8 @@
 int main(void)
 {
 char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-	putchar(ch);
-}
 for (ch = 'A' ; ch <= 'Z' ; ch++)
-{
-
-	putchar(ch);
-}
+putchar(ch);
 putchar('\n');
 return (0);
 }
