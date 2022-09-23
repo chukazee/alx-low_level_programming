43cbcfb88dbc73c20a64bad73833893a84eaee05
chukazee <chukazelibe@gmail.com>
2022-09-22 17:34:26 -0700

    final

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,46 +9,24 @@
 
 char *cap_string(char *s)
 {
-		int i = 0, j;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-			char a[] = " \t\n,;.!?\"(){}";
-
-
-
-				while (*(s + i))
-
-						{
-
-									if (*(s + i) >= 'a' && *(s + i) <= 'z')
-
-												{
-
-																if (i == 0)
-
-																					*(s + i) -= 'a' - 'A';
-
-																			else
-
-																							{
-
-																												for (j = 0; j <= 12; j++)
-
-																																	{
-
-																																							if (a[j] == *(s + i - 1))
-
-																																														*(s + i) -= 'a' - 'A';
-
-																																											}
-
-																															}
-
-																					}
-
-											i++;
-
-												}
-
-					return (s);
-
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
+			i++;
+		}
+		return (s);
 }
