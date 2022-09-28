bca388808c2f2de538010dc336fa4cd0c6763a21
chukazee <chukazelibe@gmail.com>
2022-09-27 17:28:13 -0700

    final

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,7 +11,7 @@
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
-	
+
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
@@ -22,6 +22,6 @@ char *_strpbrk(char *s, char *accept)
 		if (accept[j])
 			return (s + i);
 	}
-	
+
 	return (0);
 }
